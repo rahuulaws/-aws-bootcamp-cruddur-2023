28c46f362eb48faadf98a94ab41c5ed33f685d35
rahuulaws <rahuulshinde@gmail.com>
2023-03-02 18:29:57 +0000

    add spaces and  attributes

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,8 +1,13 @@
 from datetime import datetime, timedelta, timezone
+from opentelemetry import trace
+tracer = trace.get_tracer("home.activities")
 class HomeActivities:
   def run():
-    now = datetime.now(timezone.utc).astimezone()
-    results = [{
+    with tracer.start_as_current_span("home-activties-mock-data") :
+      span = trace.get_current_span()
+      now = datetime.now(timezone.utc).astimezone()
+      span.set_attribute("app.now", now.isoformat())
+      results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'Andrew Brown',
       'message': 'Cloud is very fun!',
@@ -41,4 +46,5 @@ class HomeActivities:
       'replies': []
     }
     ]
+    span.set_attribute("app.result_length", len(results))
     return results
