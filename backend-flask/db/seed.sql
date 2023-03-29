77594db76294f25243f9309e368e0092848049a7
rahuulaws <rahuulshinde@gmail.com>
2023-03-29 14:33:17 +0000

    Corrected Typo

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,7 +3,7 @@ INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
   ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
-  ('RA5','rahuulshinde+5@gmail.com' , 'rahuuls' ,'MOCK'),
+  ('RA5','rahuulshinde+5@gmail.com' , 'rahuuls' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
@@ -11,4 +11,4 @@ VALUES
     (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
-  )
+  );
