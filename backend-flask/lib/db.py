a7945cd81e141966d8ae03196fb88597f071c423
rahuulaws <rahuulshinde@gmail.com>
2023-04-08 16:43:06 +0000

    Messsage getting posted - Update

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -110,7 +110,6 @@ class Db:
              with conn.cursor() as cur:
                   cur.execute(sql,params)
                   json = cur.fetchone()
-                  return json[0]
 
                   if json is not None:
                     return json[0]
