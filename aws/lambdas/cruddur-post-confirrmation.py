0570772b2a01de74eb09b6879ab176f6ac15d457
rahuulaws <rahuulshinde@gmail.com>
2023-05-18 14:17:37 +0000

    updated service config.tom,cruddur-post-confirmation and backend-flask

--- aws/lambdas/cruddur-post-confirrmation.py
+++ aws/lambdas/cruddur-post-confirrmation.py
@@ -1,50 +1,58 @@
 import json
 import psycopg2
 import os
+
 def lambda_handler(event, context):
     user = event['request']['userAttributes']
     print('userAttributes')
     print(user)
-    user_display_name  = user['name']
-    user_email         = user['email']
-    user_handle        = user['preferred_username']
-    cognito_user_id    = user['sub']
+    user_display_name = user['name']
+    user_email = user['email']
+    user_handle = user['preferred_username']
+    cognito_user_id = user['sub']
+    
+    conn = None
 
     try:
-      print('entered-try')
-      sql = f"""
-         INSERT INTO public.users (
-          display_name, 
-          email,
-          handle, 
-          cognito_user_id
-          ) 
-        VALUES(
-          %(display_name)s,
-          %(email)s,
-          %(handle)s,
-          %(cognito_user_id)s
-        )
-      """
-      print('SQL Statement ----')
-      print(sql)
-      conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
-      cur = conn.cursor()
-      params = {
-        'display_name': user_display_name,
-        'email': user_email,
-        'handle': user_handle,
-        'cognito_user_id': cognito_user_id
-      }
-      cur.execute(sql,params)
-      conn.commit() 
+        print('entered-try')
+        sql = """
+            INSERT INTO public.users (
+                display_name,
+                email,
+                handle,
+                cognito_user_id
+            )
+            VALUES (
+                %(display_name)s,
+                %(email)s,
+                %(handle)s,
+                %(cognito_user_id)s
+            )
+        """
+        print('SQL Statement ----')
+        print(sql)
+        
+        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
+        cur = conn.cursor()
+        params = {
+            'display_name': user_display_name,
+            'email': user_email,
+            'handle': user_handle,
+            'cognito_user_id': cognito_user_id
+        }
+        cur.execute(sql, params)
+        conn.commit()
+    
     except (Exception, psycopg2.DatabaseError) as error:
-      print('error:')
-      print(error)
+        print('error:')
+        print(error)
+    
     finally:
-      if conn is not None:
-          cur.close()
-          conn.close()
-          print('Database connection closed.')
-
+        if conn is not None:
+            cur.close()
+            conn.close()
+            print('Database connection closed.')
+        else:
+            print('No connection to close.')
+    
     return event
