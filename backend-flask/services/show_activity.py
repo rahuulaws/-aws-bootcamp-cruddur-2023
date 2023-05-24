75cc022b753ac8de1edbc9b77f5d54ac20b51187
rahuulaws <rahuulshinde@gmail.com>
2023-05-24 09:46:45 +0000

    fixing homepage, inline replies - wip

--- backend-flask/services/show_activity.py
+++ backend-flask/services/show_activity.py
@@ -4,7 +4,7 @@ from lib.db import db
 class ShowActivity:
     def run(activity_uuid):
         sql = db.template('activities', 'show')
-        results = db.query_array_json(sql, {
+        results = db.query_object_json(sql,{
             'uuid': activity_uuid
         })
 
